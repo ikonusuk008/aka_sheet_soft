747af082b58e8987f1f9d576c728d0ab4dbb878f
ikonusuk008 <31379245+ikonusuk008@users.noreply.github.com>
2022-04-12 11:40:36 +0900

    delete comment

--- src/aka_sheet_soft/Capture.java
+++ src/aka_sheet_soft/Capture.java
@@ -1,5 +1,4 @@
 package aka_sheet_soft;
-
 import java.awt.AWTException;
 import java.awt.Rectangle;
 import java.awt.Robot;
@@ -7,132 +6,121 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
-
 class Capture {
-    private Robot robot;
 
-    private Thread thread;
-    private volatile boolean dirtyFlag;
-    private volatile boolean showFlag;
-    private volatile boolean moveFlag;
-    private volatile boolean moveFlagOld;
+   private Robot            robot;
+   private Thread           thread;
+   private volatile boolean dirtyFlag;
+   private volatile boolean showFlag;
+   private volatile boolean moveFlag;
+   private volatile boolean moveFlagOld;
+   private volatile boolean captureFlag = false;
+   private Timer            timer;
+   private CpoyScreen       parent;
 
-    private volatile boolean captureFlag = false;
+   public Capture(CpoyScreen parent) {
+      this.parent = parent;
+      try {
+         robot = new Robot();
+      } catch (AWTException ex) {
+         ex.printStackTrace();
+         return;
+      }
+   }
 
-    private Timer timer;
+   public void setShow(boolean showFlag) {
+      this.showFlag = showFlag;
+   }
 
-    private CpoyScreen parent;
+   public boolean isCapturing() {
+      return captureFlag;
+   }
 
-    public Capture(CpoyScreen parent) {
+   public void startCapturing() {
+      captureFlag = true;
+      dirtyFlag   = true;
+      moveFlagOld = true;
+      thread      = new Thread(new Runnable(){
 
-    	this.parent = parent;
+                     public void run() {
+                        try {
+                           watchDirtyRegeon();
+                        } catch (IOException e) {
+                           e.printStackTrace();
+                        }
+                     }
+                  });
+      thread.start();
+      timer = new Timer();
+      TimerTask task = new TimerTask(){
 
-        try {
-            robot = new Robot();
-        } catch (AWTException ex) {
-            ex.printStackTrace();
-            return;
-        }
-    }
-
-    public void setShow(boolean showFlag) {
-        this.showFlag = showFlag;
-    }
-
-    public boolean isCapturing() {
-        return captureFlag;
-    }
-
-    public void startCapturing() {
-        captureFlag = true;
-        dirtyFlag = true;
-        moveFlagOld = true;
-
-        thread = new Thread(new Runnable() {
-                public void run() {
-                    try {
-						watchDirtyRegeon();
-					} catch (IOException e) {
-						// TODO é©ìÆê∂ê¨Ç≥ÇÍÇΩ catch ÉuÉçÉbÉN
-						e.printStackTrace();
-					}
-                }
-            });
-
-        thread.start();
-
-        timer = new Timer();
-        TimerTask task = new TimerTask() {
-                public void run() {
-                	System.out.println("------run");
-                    if (moveFlag) {
-                    	System.out.println("------moveFlag");
-                        moveFlagOld = moveFlag;
-                        moveFlag = false;
-                    } else if (moveFlagOld) {
-                    	System.out.println("------moveFlagOld");
-                        dirtyFlag = true;
-                        wakeup();
-                        moveFlagOld = moveFlag;
-                    }
-                }
-            };
-        timer.schedule(task, 0, 1000L);
-    }
-
-    public void stopCapturing() {
-        captureFlag = false;
-
-        thread.interrupt();
-        thread = null;
-
-        timer.cancel();
-        timer = null;
-    }
-
-    private synchronized void wakeup() {
-        notifyAll();
-    }
-
-    public void notifyMove() {
-        moveFlag = true;
-    }
-
-    public void notifyPaint() {
-        if (!showFlag) {
-            dirtyFlag = true;
-            wakeup();
-        }
-    }
-
-    public BufferedImage captureScreen(Rectangle bounds) {
-        try {
-            Thread.sleep(200L);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-
-        synchronized (this) {
-            return robot.createScreenCapture(bounds);
-        }
-    }
-
-    private void watchDirtyRegeon() throws IOException {
-        Thread current = Thread.currentThread();
-        while(current == thread) {
-            if (!dirtyFlag) {
-                try {
-                    synchronized (this) {
-                        wait();
-                    }
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                    return;
-                }
-            } else {
-                parent.copy_screen();
-                dirtyFlag = false;
+         public void run() {
+            System.out.println("------run");
+            if (moveFlag) {
+               System.out.println("------moveFlag");
+               moveFlagOld = moveFlag;
+               moveFlag    = false;
+            } else if (moveFlagOld) {
+               System.out.println("------moveFlagOld");
+               dirtyFlag = true;
+               wakeup();
+               moveFlagOld = moveFlag;
             }
-        }
-    }
+         }
+      };
+      timer.schedule(task, 0, 1000L);
+   }
+
+   public void stopCapturing() {
+      captureFlag = false;
+      thread.interrupt();
+      thread = null;
+      timer.cancel();
+      timer = null;
+   }
+
+   private synchronized void wakeup() {
+      notifyAll();
+   }
+
+   public void notifyMove() {
+      moveFlag = true;
+   }
+
+   public void notifyPaint() {
+      if (!showFlag) {
+         dirtyFlag = true;
+         wakeup();
+      }
+   }
+
+   public BufferedImage captureScreen(Rectangle bounds) {
+      try {
+         Thread.sleep(200L);
+      } catch (InterruptedException ex) {
+         ex.printStackTrace();
+      }
+      synchronized (this) {
+         return robot.createScreenCapture(bounds);
+      }
+   }
+
+   private void watchDirtyRegeon() throws IOException {
+      Thread current = Thread.currentThread();
+      while (current == thread) {
+         if (!dirtyFlag) {
+            try {
+               synchronized (this) {
+                  wait();
+               }
+            } catch (InterruptedException ex) {
+               ex.printStackTrace();
+               return;
+            }
+         } else {
+            parent.copy_screen();
+            dirtyFlag = false;
+         }
+      }
+   }
 }
